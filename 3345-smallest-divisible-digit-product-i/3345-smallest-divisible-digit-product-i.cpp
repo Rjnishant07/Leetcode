273127cc4580c491c55b9a6ce32c40d078c45689
Nishant Ranjan <158460217+Rjnishant07@users.noreply.github.com>
2026-08-06 23:35:07 +0530

    Time: 0 ms (100%), Space: 8.6 MB (37.77%) - LeetHub

--- 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
@@ -7,26 +7,17 @@ public:
             int d1 = temp % 10;
             mult = mult *d1;
             temp = temp/10;
-
-        
-        }
+             }
         return mult;
     }
     int smallestNumber(int n, int t) {
-       
-       
-       
         while(true){
            int div =  divide(n);
            if(div%t==0){
-
-           
-           return n;
+             return n;
            }
            n++;
-        
         }
-       
-    return 0;
+        return 0;
     }
 };
