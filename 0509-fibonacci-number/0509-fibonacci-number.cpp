df3bfdb192719cee1be97606c7e0534915bdc4ff
Nishant Ranjan <158460217+Rjnishant07@users.noreply.github.com>
2026-08-17 18:59:49 +0530

    Time: 10 ms (45.84%), Space: 7.8 MB (38.77%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -3,9 +3,6 @@ public:
     int fib(int n) {
         if (n==0 || n==1)
             return n;
-        else 
-            return fib(n-2)+ fib(n-1);
-        
-        
+       return fib(n-2)+ fib(n-1);       
     }
 };
